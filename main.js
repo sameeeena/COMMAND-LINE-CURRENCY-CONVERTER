8f3e1333477cfceb051b8741eda9ed553d682440
sameeeena <162378926+sameeeena@users.noreply.github.com>
2024-04-27 02:19:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+let userAnswer = await inquirer.prompt([{
+        name: "From",
+        message: "enter from currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    }, {
+        name: "to",
+        message: "enter to currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "amount",
+        message: "Enter your amount",
+        type: "number"
+    }
+]);
+let fromAmount = currency[userAnswer.From];
+let toAmount = currency[userAnswer.to];
+let amount = userAnswer.amount;
+let baseAmount = amount / fromAmount; // 5000/280 exchange rate
+let convertedAmount = baseAmount * toAmount;
+console.log(Math.round(convertedAmount));
